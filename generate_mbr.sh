Linaro/poplar-l-loader

commit f692a0717e0be7c6c576af0b91fe21fa88a8d7ba

generate_mbr.sh: do some major cleanup for readability
Clean up "generate_mbr.sh":
  - use the named-fields format for input to "sfdisk"
  - assume that this script is used exclusively for Linux (not Android)
  - use bash as the shell, since bash features are used $((expr ))
  - reduce the amount of output generated during MBR creation
  - use trap to clean up the temp file even if interrupted
  - show full output of "fdisk -L mbr.bin" in comments
  - reword a few things slightly

Signed-off-by: Alex Elder <elder@linaro.org>

generate_mbr.sh
@@ -1,35 +1,38 @@
-#!/bin/sh
-# Generate partition table for Poplar eMMC
-#
-# linux: 3 entries
-PTABLE=linux-8g
-TEMP_FILE=$(mktemp /tmp/${PTABLE}.XXXXXX)
+#!/bin/bash
 
-#Poplar has 8gb eMMC
+# Generate Linux partition table for Poplar eMMC
+
+TEMP_FILE=$(mktemp /tmp/linux-8g.XXXXXX)
+trap "rm -f ${TEMP_FILE}" EXIT ERR SIGHUP SIGINT SIGQUIT SIGTERM
+
+# Poplar has 7456KB eMMC
 SECTOR_NUMBER=15269888
 SECTOR_SIZE_BYTES=512
 SIZE=$((${SECTOR_NUMBER} * ${SECTOR_SIZE_BYTES}))
 
+##### Linux on Poplar uses 3 partitions #####
+# $ fdisk -L mbr.bin
+# Disk mbr.bin: 7.3 GiB, 7818182656 bytes, 15269888 sectors
+# Units: sectors of 1 * 512 = 512 bytes
+# Sector size (logical/physical): 512 bytes / 512 bytes
+# I/O size (minimum/optimal): 512 bytes / 512 bytes
+# Disklabel type: dos
+# Disk identifier: 0xcf7e875d
+#
+# Device        Boot  Start      End  Sectors   Size Id Type
+# mbr.bin1           1     8191     8191     4M f0 Linux/PA-RISC boot
+# mbr.bin2 *      8192   287527   279336 136.4M  c W95 FAT32 (LBA)
+# mbr.bin3      288768 15269887 14981120   7.1G 83 Linux
+#####
+
 echo "Creating MBR for Poplar eMMC"
+truncate --size=${SIZE} ${TEMP_FILE}
+{
+	echo "label: dos"
+	echo "start=1      size=8191     type=f0"
+	echo "start=8192   size=279336   type=0c bootable"
+	echo "start=288768 size=14981120 type=83"
+} | sfdisk --quiet --no-reread --no-tell-kernel ${TEMP_FILE}
 
-# get the partition table
-case ${PTABLE} in
-    linux*)
-      truncate --size=$SIZE ${TEMP_FILE}
-
-#mbr.bin1             1     8191     8191     4M f0 Linux/PA-RISC boot
-#mbr.bin2   *      8192   287527   279336 136.4M  c W95 FAT32 (LBA)
-#mbr.bin3        288768 15269887 14981120   7.1G 83 Linux
-
-      sfdisk ${TEMP_FILE} <<EOF
-1, 8191, f0
-,279336, 0c *
-,, 83
-EOF
-
-      ;;
-esac
-
-#extract the mbr
-dd if=${TEMP_FILE} of=mbr.bin bs=${SECTOR_SIZE_BYTES} count=1
-rm -f ${TEMP_FILE}
+# Extract just the MBR
+dd status=none if=${TEMP_FILE} of=mbr.bin bs=${SECTOR_SIZE_BYTES} count=1

commit fe5c4c63c34369da1e44a6c21a0e08d53f58aba2

generate_mbr.sh: Remove --no-reread --no-tell-kernel sfdisk arguments
These arguments are neither portable between distros nor useful when
sfdisk is running, as it is here, on a file rather than on a device.

Fixes: f692a0717e0b ("generate_mbr.sh: do some major cleanup for
readability")
Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

generate_mbr.sh
@@ -32,7 +32,7 @@ truncate --size=${SIZE} ${TEMP_FILE}
 	echo "start=1      size=8191     type=f0"
 	echo "start=8192   size=279336   type=0c bootable"
 	echo "start=288768 size=14981120 type=83"
-} | sfdisk --quiet --no-reread --no-tell-kernel ${TEMP_FILE}
+} | sfdisk --quiet ${TEMP_FILE}
 
 # Extract just the MBR
 dd status=none if=${TEMP_FILE} of=mbr.bin bs=${SECTOR_SIZE_BYTES} count=1